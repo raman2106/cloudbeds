c4e2f0f323dc180dec1e72592a8c531a9130ad55
Raman Kumar Mahapatra <raman.mahapatra@gmail.com>
2023-05-08 08:12:39 +0530

    Modified the script to print the client's IP to the console

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def greet():
+def home():
+  #Log the client's IP'
+  client_ip = request.remote_addr
+  print(f'Client IP:{client_ip}')#Improvement: Should we log it to a log file?
   return render_template("home.html")
 
 
