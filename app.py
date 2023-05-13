8ea84cc9e04e7bbcb6003a5fecab5c0c95a89956
Raman Kumar Mahapatra <raman.mahapatra@gmail.com>
2023-05-13 17:02:42 +0530

    Reimplemented the login, 400 error, and 500 error pages with jinja template

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def home():
   #Log the client's IP'
   client_ip = request.remote_addr
   print(f'Client IP:{client_ip}')#Improvement: Should we log it to a log file?
-  return render_template("home.html")
+  return render_template("login.html")
 
 #Invalid URL
 @app.errorhandler(404)
