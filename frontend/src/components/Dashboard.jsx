f7094cb609bd530ecc093791f91f2c0fe9b6e685
Raman Kumar Mahapatra <raman.mahapatra@gmail.com>
2024-05-01 23:37:30 +0530

    Implemented Dashboard cardw widget

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,48 +1,75 @@
-import React from 'react';
-import {
-    Menu,
-    MenuButton,
-    MenuList,
-    MenuItem,
-    MenuItemOption,
-    MenuGroup,
-    MenuOptionGroup,
-    MenuDivider, 
-    IconButton
-  } from '@chakra-ui/react'
-import { AddIcon, HamburgerIcon, ExternalLinkIcon, RepeatIcon, EditIcon } from "@chakra-ui/icons";
-import { Router, Route, Routes, Link as RouterLink, Navigate } from 'react-router-dom'; // Import Link as RouterLink
+import { 
+    Card, 
+    CardHeader, 
+    CardBody,  
+    SimpleGrid, 
+    Flex, 
+    Heading, 
+    Text, 
+    Button,
+    Table,
+    Thead,
+    Tbody,
+    Tfoot,
+    Tr,
+    Th,
+    Td,
+    TableCaption,
+    TableContainer} from '@chakra-ui/react'
 
-// import Content1 from './Content1';
-// import Content2 from './Content2';
-import DefaultContent from './DefaultContent'; // Import DefaultContent component
 
 const Dashboard = () => {
     return (
-            <Menu>
-            <MenuButton
-                as={IconButton}
-                aria-label='Options'
-                icon={<HamburgerIcon />}
-                variant='outline'
-                defaultIsOpen={true}
-            />
-            <MenuList>
-                <MenuItem icon={<AddIcon />} command='⌘T'>
-                New Tab
-                </MenuItem>
-                <MenuItem icon={<ExternalLinkIcon />} command='⌘N'>
-                New Window
-                </MenuItem>
-                <MenuItem icon={<RepeatIcon />} command='⌘⇧N'>
-                Open Closed Tab
-                </MenuItem>
-                <MenuItem icon={<EditIcon />} command='⌘O'>
-                Open File...
-                </MenuItem>
-            </MenuList>
-            </Menu>
-        );
-};
+        <Flex justifyContent="center" alignItems="start" height="100vh">
+            <SimpleGrid spacing={4} templateColumns='repeat(3, 1fr)'  pl={64}>
+                <Card  
+                    w="300px" 
+                    h="150px" 
+                    boxShadow="lg" 
+                    borderRadius="md" 
+                    border="1px solid" 
+                    borderColor="gray.200" 
+                    backgroundColor="gray.85">
+                    <CardHeader p={0}>
+                    <Heading size='sm' pl={4} pt={4}>Booked</Heading>
+                    </CardHeader>
+                    <CardBody p={0} textAlign="center">
+                    <Text fontFamily="mono" fontSize="7xl" fontWeight="bold">22</Text>
+                    </CardBody>
+                </Card>
+                <Card
+                    w="300px" 
+                    h="150px" 
+                    boxShadow="lg" 
+                    borderRadius="md" 
+                    border="1px solid" 
+                    borderColor="gray.200" 
+                    backgroundColor="gray.85">
+                    <CardHeader p={0}>
+                        <Heading size='sm' pl={4} pt={4}>Ongoing</Heading>
+                    </CardHeader>
+                    <CardBody p={0} textAlign="center">
+                        <Text fontFamily="mono" fontSize="7xl" fontWeight="bold">02</Text>
+                    </CardBody>
+                </Card>
+                <Card
+                    w="300px" 
+                    h="150px" 
+                    boxShadow="lg" 
+                    borderRadius="md" 
+                    border="1px solid" 
+                    borderColor="gray.200" 
+                    backgroundColor="gray.85">
+                    <CardHeader p={0}>
+                        <Heading size='sm' pl={4} pt={4}>Cancelled</Heading>
+                    </CardHeader>
+                    <CardBody p={0} textAlign="center">
+                        <Text fontFamily="mono" fontSize="7xl" fontWeight="bold">10</Text>
+                    </CardBody>
+                </Card>
+            </SimpleGrid>
+        </Flex>
+    );
+  };
 
 export default Dashboard;
